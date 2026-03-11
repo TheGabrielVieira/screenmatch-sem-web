cdcf182dbd27238ae8e26761c326acdab0add1b2
Gabriel <50024453+TheGabrielVieira@users.noreply.github.com>
2026-03-11 15:42:20 -0300

    feat: adicionando nova dependencia, configurando o cors, fazendo alteração nas dependencias do controller e service para serie, e aplicando camada de dto para serie

--- src/main/java/br/com/screenmatch/ProjectscreenmatchApplication.java
+++ src/main/java/br/com/screenmatch/ProjectscreenmatchApplication.java
@@ -1,9 +1,5 @@
 package br.com.screenmatch;
 
-import br.com.screenmatch.main.NovaMain;
-import br.com.screenmatch.repository.SerieRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
